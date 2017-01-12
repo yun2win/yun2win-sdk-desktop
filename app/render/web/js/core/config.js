29cfbe0926ca76fc3d8b615808db5a459e41364d
yun2win <yun2win@liyueyun.com>
2017-01-12 12:00:14 +0800

    Update config.js

--- app/render/web/js/core/config.js
+++ app/render/web/js/core/config.js
@@ -2,5 +2,5 @@ function config(){
 }
 
 //config.baseUrl = 'http://localhost:18081/v1/';
-config.baseUrl = 'http://im.yun2win.com/v1/';
+config.baseUrl = 'http://console.yun2win.com:8080/v1/';
 config.y2wAutorizeUrl = 'http://console.yun2win.com/';
